e40da1a5722cf889c7ef449aa2808e61189105b9
akshataw <wagheakshu@gmail.com>
2018-12-20 18:03:55 +0530

    Minor change

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
-const http = require('http');
-const fs = require('fs');
+// github code
+const http = require("http");
+const fs = require("fs");
 
 const getDate = date => ({
   unix: date.getTime(),
@@ -7,54 +8,49 @@ const getDate = date => ({
 });
 
 const requestHandler = (req, res) => {
-  //console.log(req.url);
-  if(req.url === '/'){
-    fs.readFile("views/index.html", "utf-8", (err, html) => {
-      if(err){
-        console.log(err);
-      }
-      res.writeHead(200, {"Content-Type": "text/html"});
+  if (req.url === "/") {
+    fs.readFile("views/index.html", (err, html) => {
+      if (err) throw err;
+
+      res.writeHead(200, { "Content-Type": "text/html" });
       res.end(html);
     });
-  }
-  else if(req.url.startsWith("/api/timestamp")){
-    const dateString = req.url.split("/api/timestamp")[1];
+  } else if (req.url.startsWith("/api/timestamp")) {
+    const dateString = req.url.split("/api/timestamp/")[1];
     let timestamp;
-    if(dateString === undefined || dateString.trim() === ""){
-      timestamp = getDate(new Date());
-    }
-    else{
-      const date = !isNaN(dateString)
-         ? new Date(parseInt(dateString))
-         : new Date(dateString);
 
-      if(!isNaN(date.getTime())){
+    if (dateString === undefined || dateString.trim() === "") {
+      timestamp = getDate(new Date());
+    } else {
+      const date = !isNaN(dateString)
+        ? new Date(parseInt(dateString))
+        : new Date(dateString);
+
+      if (!isNaN(date.getTime())) {
         timestamp = getDate(date);
-      }
-      else{
+      } else {
         timestamp = {
           error: "Invalid Date"
         };
       }
-      }
-      res.writeHead(200, {'Content-Type': 'application/json'});
-      res.end(JSON.stringify(timestamp));
     }
-    else{
-      fs.readFile("views/error.html", (err, html) => {
-        if(err){
-          console.log(err);
-        }
-        res.writeHead(404, {"Content-Type": "text/html"});
-        res.end(html);
-      });
-    }
-  };
+
+    res.writeHead(200, { "Content-Type": "application/json" });
+    res.end(JSON.stringify(timestamp));
+  } else {
+    fs.readFile("views/error.html", (err, html) => {
+      if (err) throw err;
+
+      res.writeHead(404, { "Content-Type": "text/html" });
+      res.end(html);
+    });
+  }
+};
 
 const server = http.createServer(requestHandler);
 
-server.listen(process.env.PORT || 8080, err => {
-  if(err) throw err;
+server.listen(process.env.PORT || 4100, err => {
+  if (err) throw err;
 
-  console.log(`Server is listening on PORT ${server.address().port}`);
+  console.log(`Server running on PORT ${server.address().port}`);
 });
