c9e520d64d8ffb69afc9376edeab03a35292acef
akshataw <wagheakshu@gmail.com>
2018-12-20 15:12:05 +0530

    Initial commit

---
+++ main.js
@@ -0,0 +1,60 @@
+const http = require('http');
+const fs = require('fs');
+
+const getDate = date => ({
+  unix: date.getTime(),
+  utc: date.toUTCString()
+});
+
+const requestHandler = (req, res) => {
+  //console.log(req.url);
+  if(req.url === '/'){
+    fs.readFile("views/index.html", "utf-8", (err, html) => {
+      if(err){
+        console.log(err);
+      }
+      res.writeHead(200, {"Content-Type": "text/html"});
+      res.end(html);
+    });
+  }
+  else if(req.url.startsWith("/api/timestamp")){
+    const dateString = req.url.split("/api/timestamp")[1];
+    let timestamp;
+    if(dateString === undefined || dateString.trim() === ""){
+      timestamp = getDate(new Date());
+    }
+    else{
+      const date = !isNaN(dateString)
+         ? new Date(parseInt(dateString))
+         : new Date(dateString);
+
+      if(!isNaN(date.getTime())){
+        timestamp = getDate(date);
+      }
+      else{
+        timestamp = {
+          error: "Invalid Date"
+        };
+      }
+      }
+      res.writeHead(200, {'Content-Type': 'application/json'});
+      res.end(JSON.stringify(timestamp));
+    }
+    else{
+      fs.readFile("views/error.html", (err, html) => {
+        if(err){
+          console.log(err);
+        }
+        res.writeHead(404, {"Content-Type": "text/html"});
+        res.end(html);
+      });
+    }
+  };
+
+const server = http.createServer(requestHandler);
+
+server.listen(process.env.PORT || 8080, err => {
+  if(err) throw err;
+
+  console.log(`Server is listening on PORT ${server.address().port}`);
+});
